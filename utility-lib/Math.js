92a1da651b9a160eed9c3b0a8816912e59c18c7e
AgilanD <agilan.d@mitrahsoft.in>
2026-03-16 13:20:24 +0530

    Function

---
+++ utility-lib/Math.js
@@ -0,0 +1,15 @@
+ function add (num,num1){
+     return num+num1
+ }
+
+ function subtract(num,num1){
+    return Math.abs(num,num1);
+ }
+
+  function multiply(num,num1){
+    return num*num1;
+ }
+
+ console.log(add(10,20));
+ console.log(subtract(10,20));
+ console.log(multiply(10,20));
