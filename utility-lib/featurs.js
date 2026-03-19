b999108cf0ebb71ef599a83fd60939063a2ca34b
AgilanD <agilan.d@mitrahsoft.in>
2026-03-19 13:08:44 +0530

    lower Function created

--- utility-lib/featurs.js
+++ utility-lib/featurs.js
@@ -5,6 +5,14 @@ function capitalize(str){
    return "The input is not a String";
 }
 
+function lower(str){
+   if(typeof str =='string'){
+    return str.toLowerCase();
+   }
+   return "The input is not a String";
+}
+
 console.log(capitalize("agilan"));
 console.log(capitalize("1234"));
 console.log(capitalize(12345));
+console.log(lower("AGILAN"));
